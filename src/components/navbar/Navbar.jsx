75b1810a2c9ff7d1db714173f86afef82c7bd277
Zülal Cengiz <zc.swe@gmail.com>
2024-07-27 17:35:41 +0300

    first commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,26 @@
+import "./navbar.scss";
+
+function Navbar() {
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo-freepik.png" width={"25px"} />
+          <span>Z-Estate</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+      </div>
+      <div className="right">
+        <a href="/">Login</a>
+        <a href="/" className="register">
+          Sign up
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
