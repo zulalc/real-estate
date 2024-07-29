82daa367e246626ba38f5a29d51c982d926300dd
Zülal Cengiz <zc.swe@gmail.com>
2024-07-29 22:52:10 +0300

    Home page

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import "./Home.scss";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="text-container">
+        <div className="img-container"></div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
