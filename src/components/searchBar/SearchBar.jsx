62cd89a7c46423a7646e65cb03a0a02e762b8529
Zülal Cengiz <zc.swe@gmail.com>
2024-08-02 21:55:13 +0300

    Search bar

---
+++ src/components/searchBar/SearchBar.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import "./SearchBar.scss";
+
+const types = ["buy", "rent"];
+
+function SearchBar() {
+  const [query, setQuery] = useState({
+    type: "buy",
+    location: "",
+    minPrice: 0,
+    maxPrice: 0,
+  });
+
+  const changeType = (value) => {
+    setQuery((prev) => ({ ...prev, type: value }));
+  };
+  return (
+    <div className="searchBar">
+      <div className="type">
+        {types.map((type) => (
+          <button
+            key={type}
+            onClick={() => changeType(type)}
+            className={query.type === type ? "active" : ""}
+          >
+            {type}
+          </button>
+        ))}
+      </div>
+      <form>
+        <input type="text" name="location" placeholder="City Location" />
+        <input
+          type="number"
+          name="minPrice"
+          min={0}
+          max={1000000}
+          placeholder="Min Price"
+        />
+        <input
+          type="number"
+          name="maxPrice"
+          min={0}
+          max={1000000}
+          placeholder="Max Price"
+        />
+        <button>
+          <img src="/search.png" alt="" />
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default SearchBar;
