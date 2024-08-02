5a68950caabc651445f511a68fc112b952f21287
Zülal Cengiz <zc.swe@gmail.com>
2024-08-02 21:57:44 +0300

    Home initial design

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,30 @@
+import SearchBar from "../../components/searchBar/SearchBar";
 import "./Home.scss";
 
 function Home() {
   return (
     <div className="home">
       <div className="text-container">
-        <div className="img-container"></div>
+        <div className="wrapper">
+          <h1 className="title">Find Your Dream Home</h1>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>10+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+
+            <div className="box">
+              <h1>500+</h1>
+              <h2>Successful Property Sales</h2>
+            </div>
+
+            <div className="box">
+              <h1>15</h1>
+              <h2>Industry Awards</h2>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
