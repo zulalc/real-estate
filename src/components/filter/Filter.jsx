425d5d5afb951ee85e265242070e709258c159ac
Zülal Cengiz <zc.swe@gmail.com>
2024-08-07 14:48:03 +0300

    Filter page

---
+++ src/components/filter/Filter.jsx
@@ -0,0 +1,80 @@
+import "./Filter.scss";
+
+function Filter() {
+  return (
+    <div className="filter">
+      <h1>
+        Search results for <b>London</b>
+      </h1>
+      <div className="top">
+        <div className="item">
+          <label htmlFor="city">Location</label>
+          <input
+            type="text"
+            name="city"
+            id="city"
+            placeholder="City Location"
+          />
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="item">
+          <label htmlFor="type">Type</label>
+          <select name="type" id="type">
+            <option value="">Any</option>
+            <option value="buy">Buy</option>
+            <option value="rent">Rent</option>
+          </select>
+        </div>
+        <div className="item">
+          <label htmlFor="property">Property</label>
+          <select name="property" id="property">
+            <option value="">Any</option>
+            <option value="apartment">Apartment</option>
+            <option value="house">House</option>
+            <option value="condo">Condo</option>
+            <option value="land">Land</option>
+          </select>
+        </div>
+        <div className="item">
+          <label htmlFor="minPrice">Min Price</label>
+          <input
+            type="number"
+            name="minPrice"
+            id="minPrice"
+            placeholder="Any"
+            min={0}
+            max={1000000}
+          />
+        </div>
+        <div className="item">
+          <label htmlFor="maxPrice">Max Price</label>
+          <input
+            type="number"
+            name="maxPrice"
+            id="maxPrice"
+            placeholder="Any"
+            min={0}
+            max={1000000}
+          />
+        </div>
+        <div className="item">
+          <label htmlFor="bedroom">Bedroom</label>
+          <input
+            type="number"
+            name="bedroom"
+            id="bedroom"
+            placeholder="Any"
+            min={0}
+            max={1000000}
+          />
+        </div>
+        <button>
+          <img src="/search.png" alt="" />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Filter;
